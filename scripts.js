3a5f4731317680126249e62bdc67db3076dfda52
mkdesign <me@mkdesign.ir>
2013-03-05 00:32:54 +0330

    menu click animates

--- scripts.js
+++ scripts.js
@@ -3,19 +3,52 @@ $(function(){
 	var trigger=$('footer span.trigger');
 
 	trigger.click(function(){
+		if($('footer').css('bottom')==0+'px'){
+			$('footer').animate({
 
-		$('footer').animate({
+				bottom : -270
 
-			bottom : 0
+			},900)
+		}else{
+			$('footer').animate({
 
-		},900,null,function(){
-			$(this).click(function(){
-				$('footer').animate({
-					bottom:-270
-				},500)
-			})
-		})	
+				bottom : 0
+
+			},900)
+		}	
+	})
+	var menus = $('nav.main-menu menu li'),
+		pin   = $('header span.pin'),
+		train = $('div.train'),
+		wheel = $('span.wheel'),
+		zoom  = $('div.show-content');
+
+	menus.click(function(){
+		var thisItem = $(this).index();
+
+		if(thisItem>=3){
+			pin.animate({
+				left:320+thisItem*100
+			},3000)
+		}else{
+			pin.animate({
+				left:80+thisItem*100
+			},3000)
+		}
+		train.animate({
+			'-webkit-transform':'rotate3d(0,0,1,'+(thisItem*60)+'deg)'
+		},3000);
+		wheel.animate({
+			'-webkit-transform':'rotate3d(0,0,1,'+(thisItem*60)+'deg)'
+		},3000);
+		zoom.addClass('zoomoutin');
+		
+
+		setTimeout(function(){
+			zoom.removeClass('zoomoutin');
+		},3000)
 
 	})
+
 	
 })
